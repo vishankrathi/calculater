53c4cccbbe9c1369f73889263e62eb8135083f3e
vishankrathi <129822733+vishankrathi@users.noreply.github.com>
2024-01-21 09:56:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let input=document.getElementById('input');
 let buttons=document.querySelectorAll('button');
 let string="";
 let arr = Array.from(buttons);
-arr.forEach(button=> {
+arr.forEach(button=> 
     button.addEventListener('click',(e)=>{
         if(e.target.innerHTML =='='){
             string = eval(string);
